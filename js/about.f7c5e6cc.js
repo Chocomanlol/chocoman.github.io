1206ea64a334494e8909b0f383706c5a0960503d
Chocomanlol <89986997+Chocomanlol@users.noreply.github.com>
2021-09-04 00:43:32 +0700

    Delete js directory

--- js/about.f7c5e6cc.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var c=n("7a23"),a={class:"about"},o=Object(c["e"])("h1",null,"what the hell",-1),u=[o];function s(t,e){return Object(c["q"])(),Object(c["d"])("div",a,u)}const b={};b.render=s;e["default"]=b}}]);
-//# sourceMappingURL=about.f7c5e6cc.js.map
